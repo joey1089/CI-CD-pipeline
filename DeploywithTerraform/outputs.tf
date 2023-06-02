4d02b98a48ab12c269b3fc4d681acade379582cd
Joseph Peter <josephvpeter7@gmail.com>
2023-06-02 19:46:27 -0400

    updated code in several files

--- DeploywithTerraform/outputs.tf
+++ DeploywithTerraform/outputs.tf
@@ -1,10 +1,16 @@
 #----------- root/outputs.tf ------------------
 
 
-output "s3_bucket_uri" {
-  description = "S3 bucket URI"
-  value       = "s3://${aws_s3_bucket.s3.id}"
-}
+# output "s3_bucket_uri" {
+#   description = "S3 bucket URI"
+#   value       = "s3://${aws_s3_bucket.s3-bucket-jenkins-artifacts.id}"
+    #Error- References in depends_on must be to a whole object (resource, etc), not to an attribute of an object.
+# }
+
+# output "s3_bucket_url" {
+#   description = "S3 Bucket URL"
+#   value = "https://${aws_s3_bucket.s3-bucket-jenkins-artifacts.id}"
+# }
 
 output "instance_public_ip" {
   value = aws_instance.ec2_instance.public_ip
--- DeploywithTerraform/variables.tf
+++ DeploywithTerraform/variables.tf
@@ -12,10 +12,40 @@ variable "ssh_key_name" {
   default = "key-pair-jens"
 }
 
-variable "ec2_user_data" {
+variable "user_data" {
   description = "User data script for EC2"
   type        = string
-  default     = "user-data.sh"
+  default     = <<EOF
+                        #!/bin/bash
+                        # Install Jenkins and Java 
+                        sudo wget -O /etc/yum.repos.d/jenkins.repo \
+                            https://pkg.jenkins.io/redhat-stable/jenkins.repo
+                        sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+                        sudo yum upgrade -y
+                        # Add required dependencies for the jenkins package
+                        sudo amazon-linux-extras install -y java-openjdk11 
+                        sudo yum install -y jenkins
+                        sudo systemctl daemon-reload
+
+                        # Start Jenkins
+                        sudo systemctl enable jenkins
+                        sudo systemctl start jenkins
+
+                        # Firewall Rules
+                        if [[ $(firewall-cmd --state) = 'running' ]]; then
+                            YOURPORT=8080
+                            PERM="--permanent"
+                            SERV="$PERM --service=jenkins"
+
+                            firewall-cmd $PERM --new-service=jenkins
+                            firewall-cmd $SERV --set-short="Jenkins ports"
+                            firewall-cmd $SERV --set-description="Jenkins port exceptions"
+                            firewall-cmd $SERV --add-port=$YOURPORT/tcp
+                            firewall-cmd $PERM --add-service=jenkins
+                            firewall-cmd --zone=public --add-service=http --permanent
+                            firewall-cmd --reload
+                        fi
+                  EOF
 }
 
 variable "ec2_tag" {
@@ -63,7 +93,7 @@ variable "ec2-s3-permissions" {
 }
 
 variable "bucket_name" {
-  default = "custom-s3-bucket-4-jenkins-artifacts"
+  default = "s3-bucket-jenkins-artifacts"
 }
 
 variable "environment" {
