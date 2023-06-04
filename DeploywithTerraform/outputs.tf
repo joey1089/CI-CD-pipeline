e371c78a11e0ad5e825e05e6bc5ab9dcd4655daa
Joseph Peter <josephvpeter7@gmail.com>
2023-06-04 18:41:40 -0400

    added gp3 in main and udpated code

--- DeploywithTerraform/outputs.tf
+++ DeploywithTerraform/outputs.tf
@@ -2,12 +2,12 @@
 
 output "ec2_remote_access" {
   description = "EC2 Remote Access"
-  value       = "ssh -i ${local_file.private_key_pem.filename} ec2-user@${aws_instance.ec2_instance.public_ip}"
+  value       = "ssh -i ${local_file.private_key_pem.filename} ec2-user@${aws_instance.amazon_instance.public_ip}"
 }
 
 output "instance_public_ip" {
   description = "Public IP address of the Jenkins EC2 instance"
-  value       = "Jenkins Server Public IP: ${aws_instance.ec2_instance.public_ip}"
+  value       = "Jenkins Server Public IP: ${aws_instance.amazon_instance.public_ip}"
 }
 
 output "s3_bucket_uri" {
